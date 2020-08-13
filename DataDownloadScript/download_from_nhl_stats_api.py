a21c7175294b4a7f3037bc59891b616f48c25720
Andrew DeBiase <debiaseap@gmail.com>
2020-08-13 00:55:24 -0400

    Set up api urls

--- DataDownloadScript/download_from_nhl_stats_api.py
+++ DataDownloadScript/download_from_nhl_stats_api.py
@@ -2,6 +2,7 @@ import numpy as np
 import requests
 import argparse
 import json
+from datetime import datetime
 
 ### PARSER ###
 # Get fantasy point multiplier values from the command line.
@@ -70,18 +71,65 @@ so_multiplier   = args.so
 otl_multiplier  = args.otl
 
 
+### CONSTANT STRINGS ###
+skater_summary = 'summary'
+skater_fo_percentage = 'faceoffpercentages'
+skater_fo_wl = 'faceoffwins'
+skater_gfga = 'goalsForAgainst'
+skater_misc = 'realtime'
+skater_penalties = 'penalties'
+skater_pk = 'penaltykill'
+skater_pp = 'powerplay'
+skater_puck_possession = 'puckPossessions'
+skater_sat_count = 'summaryshooting'
+skater_sat_percentages = 'percentages'
+skater_scoring_per_60 = 'scoringRates'
+skater_scoring_per_game = 'scoringpergame'
+skater_toi = 'timeonice'
+
+goalie_summary = 'summary'
+goalie_advanced = 'advanced'
+goalie_saves_by_strength = 'savesByStrength'
+
+left_wing_tag = 'L'
+right_wing_tag = 'R'
+center_tag = 'C'
+defenceman_tag = 'D'
+
+year_upper_bound = f'{datetime.now().year - 1}{datetime.now().year}'
+year_lower_bound = f'{datetime.now().year - 5}{datetime.now().year - 4}'
+
+report = ''
+position_tag = ''
+
+
 ### DATA SCRAPING ###
 # Download data from the API the NHL uses for nhl.com/stats
-url_forwards = 'https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=[%7B%22property%22:%22lastName%22,%22direction%22:%22ASC_CI%22%7D,%7B%22property%22:%22skaterFullName%22,%22direction%22:%22ASC_CI%22%7D]&start=100&limit=101&factCayenneExp=gamesPlayed%3E=30&cayenneExp=(positionCode=%22L%22%20or%20positionCode=%22R%22%20or%20positionCode=%22C%22)%20and%20active=1%20and%20gameTypeId=2%20and%20seasonId%3C=20192020%20and%20seasonId%3E=20152016'
+base_skater_url = 'https://api.nhle.com/stats/rest/en/skater/{}?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22lastName%22,%22direction%22:%22ASC_CI%22%7D,%7B%22property%22:%22skaterFullName%22,%22direction%22:%22ASC_CI%22%7D%5D&start=0&limit=50&factCayenneExp=gamesPlayed%3E=1%20and%20gamesPlayed%3E=25&cayenneExp=active%3D1%20and%20gameTypeId=2%20and%20positionCode%3D%22{}%22%20and%20seasonId%3C={}%20and%20seasonId%3E={}'
+base_goalie_url = 'https://api.nhle.com/stats/rest/en/goalie/{}?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22lastName%22,%22direction%22:%22ASC_CI%22%7D,%7B%22property%22:%22savePct%22,%22direction%22:%22DESC%22%7D%5D&start=0&limit=50&factCayenneExp=gamesPlayed%3E=15&cayenneExp=active%3D1%20and%20gameTypeId=2%20and%20seasonId%3C={}%20and%20seasonId%3E={}'
 
-forward_records = requests.get(url_forwards).json()
+
+
+
+
+records = requests.get(base_skater_url.format(skater_summary, left_wing_tag, year_upper_bound, year_lower_bound)).json()
 
 # todo: increment the start in the url each time by 100 so we don't get the same 100 players every time
 # todo: add url for defensement and forwards
 # todo: set date range to be last 5 years by getting the previous year rather than hard-coding it
 
 for i in range(int(records['total']/100)+1):
-    players = records['data']
+	players = records['data']
+	print(players)
     # todo: concatenate the json data together in a numpy array
 
 # todo: save a .npy file for each forward, defense, and goalie and save the files to the correct folders in /data
+
+
+#get 5 season prior to this season loops
+#-get current year then use previos 5 years
+#-add argument for length to go back
+	#url_forward temp string
+	#formulate api request
+	#get data
+	#change year
