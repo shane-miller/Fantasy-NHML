d0e96361bbdb9cf48b8b9fdad2b34fe6d324eda6
Shane Miller <shanekmiller44@gmail.com>
2020-09-09 00:08:16 -0400

    Added vanilla networks for all positions

--- VanillaDeepNetwork/ReportGeneration/GenerationScripts/goalie_report.py
+++ VanillaDeepNetwork/ReportGeneration/GenerationScripts/goalie_report.py
@@ -25,20 +25,16 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.fc1 = nn.Linear(190, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.Linear(64, 32)
-        self.fc5 = nn.Linear(32, 16)
-        self.fc6 = nn.Linear(16, 1)
+        self.fc1 = nn.Linear(38, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 16)
+        self.fc4 = nn.Linear(16, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
-        x = self.fc6(x)
+        x = self.fc4(x)
 
         return x
 
