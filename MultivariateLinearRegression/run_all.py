1d7762cf134dd751c873ae09d924f160749913cc
Shane Miller <shanekmiller44@gmail.com>
2020-09-15 06:00:41 -0400

    Fixed run_all's to be able to be called from elsewhere

--- MultivariateLinearRegression/run_all.py
+++ MultivariateLinearRegression/run_all.py
@@ -1,28 +1,33 @@
-from Networks import center_network
-from Networks import wing_network
-from Networks import defenceman_network
-from Networks import goalie_network
-from ReportGeneration import generate_all_reports
+from MultivariateLinearRegression.Networks import center_network
+from MultivariateLinearRegression.Networks import wing_network
+from MultivariateLinearRegression.Networks import defenceman_network
+from MultivariateLinearRegression.Networks import goalie_network
+from MultivariateLinearRegression.ReportGeneration import generate_all_reports
 from shutil import rmtree
 import pathlib
 
-print('----- Processing Multivariate Linear Regression -----')
-##### Generate Models #####
-center_network.main()
-wing_network.main()
-defenceman_network.main()
-goalie_network.main()
+def main():
+    print('----- Processing Multivariate Linear Regression -----')
+    ##### Generate Models #####
+    center_network.main()
+    wing_network.main()
+    defenceman_network.main()
+    goalie_network.main()
 
 
-##### Generate Reports #####
-generate_all_reports.main()
+    ##### Generate Reports #####
+    generate_all_reports.main()
 
 
-##### Remove __pycache__ Folders #####
-current_file_path = pathlib.Path(__file__).parent.absolute()
+    ##### Remove __pycache__ Folders #####
+    current_file_path = pathlib.Path(__file__).parent.absolute()
 
-path = current_file_path / 'Networks' / '__pycache__'
-rmtree(path)
+    path = current_file_path / 'Networks' / '__pycache__'
+    rmtree(path)
 
-path = current_file_path / 'ReportGeneration' / '__pycache__'
-rmtree(path)
+    path = current_file_path / 'ReportGeneration' / '__pycache__'
+    rmtree(path)
+
+
+if __name__ == "__main__":
+    main()
