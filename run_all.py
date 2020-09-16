63c75edb2a49245552e4e957ed1b5628235b4619
Shane Miller <shanekmiller44@gmail.com>
2020-09-16 00:09:34 -0400

    Changed run all to give you the option to run only specific models

--- run_all.py
+++ run_all.py
@@ -1,24 +1,62 @@
 from MultivariateLinearRegression import run_all as mlr
 from GradientBoost import run_all as gb
-from AdaBoost import run_all as ab
 from RandomForest import run_all as rf
+from AdaBoost import run_all as ab
 from XGBoost import run_all as xgb
 from shutil import rmtree
+import argparse
 import pathlib
 
+
 def main():
+    ##### PARSER #####
+    parser = argparse.ArgumentParser(description = 'Use these if you don\'t want every model to run.')
+
+    parser.add_argument('--mlr', type = bool, default = False, help = 'Add if you want to run MultivariateLinearRegression')
+    parser.add_argument('--rf',  type = bool, default = False, help = 'Add if you want to run RandomForest')
+    parser.add_argument('--ab',  type = bool, default = False, help = 'Add if you want to run AdaBoost')
+    parser.add_argument('--gb',  type = bool, default = False, help = 'Add if you want to run GradientBoost')
+    parser.add_argument('--xgb', type = bool, default = False, help = 'Add if you want to run XGBoost')
+
+    args = parser.parse_args()
+
+    run_mlr = args.mlr
+    run_rf  = args.rf
+    run_ab  = args.ab
+    run_gb  = args.gb
+    run_xgb = args.xgb
+
+
     ##### Run All Networks #####
+    if not (run_mlr or run_rf or run_ab or run_gb or run_xgb):
+        run_mlr = True
+        run_rf  = True
+        run_ab  = True
+        run_gb  = True
+        run_xgb = True
+
+    if run_mlr:
+        print()
+        mlr.main()
+
+    if run_rf:
+        print()
+        rf.main()
+
+    if run_ab:
+        print()
+        ab.main()
+
+    if run_gb:
+        print()
+        gb.main()
+
+    if run_xgb:
+        print()
+        xgb.main()
+
     print()
-    mlr.main()
-    print()
-    rf.main()
-    print()
-    ab.main()
-    print()
-    gb.main()
-    print()
-    xgb.main()
-    print()
+
 
     ##### Remove __pycache__ Folders #####
     current_file_path = pathlib.Path(__file__).parent.absolute()
